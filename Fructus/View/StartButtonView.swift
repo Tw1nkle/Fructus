10fecc64848680b53780aedac1d47d1fe0d2bd8d
Daria Fediashova <d.fedyashova@bk.ru>
2021-02-18 19:32:22 +0300

    Created new files and building OnboardingView with list fruits

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,39 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Дарья Федяшова on 18.02.2021.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.25))
+        } //: BUTTON
+        .accentColor(Color.white)
+    }
+}
+
+// MARK: - VIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
