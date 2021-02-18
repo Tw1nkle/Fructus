d647013b42c7e0430ae766190c87fb2516c465f8
Daria Fediashova <d.fedyashova@bk.ru>
2021-02-18 16:43:47 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Дарья Федяшова on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
